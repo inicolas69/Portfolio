b705950ce5db0ac3f6f37203936d8befbabe316a
Nicolas Iniesta <inicolas69@gmail.com>
2020-05-23 21:13:55 +0200

    Texte modifié, logo, font

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ navLinks.forEach((link) => {
 // Dark theme
 
 const darkToggle = document.querySelector('.dark');
+const logo = document.querySelector('.logo');
 
 if (localStorage.getItem('theme') == 'sombre') {
   modeSombre();
@@ -23,11 +24,13 @@ if (localStorage.getItem('theme') == 'sombre') {
 
 function modeSombre() {
   document.body.className = 'dark-theme';
+  logo.innerHTML = `<img src="img/logo-text-white.png" alt="logo" />`;
 }
 
 function changerTheme() {
   if (document.body.classList.contains('dark-theme')) {
     document.body.className = '';
+    logo.innerHTML = `<img src="img/logo-text-black.png" alt="logo" />`;
     localStorage.setItem('theme', 'clair');
   } else {
     modeSombre();
@@ -38,8 +41,6 @@ function changerTheme() {
 darkToggle.addEventListener('click', changerTheme);
 
 // Anim logo
-
-const logo = document.querySelector('.logo');
 
 logo.addEventListener('click', () => {
   document.body.classList.toggle('clic-logo');
