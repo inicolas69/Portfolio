fcb82bad592785127a86136fa8fff80024266eb3
Nicolas Iniesta <inicolas69@gmail.com>
2020-05-24 15:10:58 +0200

    Ajout animation texte

--- main.js
+++ main.js
@@ -45,3 +45,29 @@ darkToggle.addEventListener('click', changerTheme);
 logo.addEventListener('click', () => {
   document.body.classList.toggle('clic-logo');
 });
+
+// Anim texte
+
+new TypeIt('#anim', {
+  //   strings: ['Bonjour,', 'Salut,', 'Hey,'],
+  speed: 80,
+  loop: false,
+  startDelete: true,
+  breakLines: false,
+  lifeLike: true,
+})
+  .type(`Bonjour, moi c'est <strong>Nicolas&nbspIniesta</strong>`, {
+    delay: 400,
+  })
+  .move(-27, { speed: 25, delay: 400 })
+  .delete(8)
+  .type('Hey !', { delay: 500 })
+  .move(3, { speed: 25, delay: 400 })
+  .delete(3)
+  .type(' M', { delay: 500, speed: 120 })
+  .pause(500)
+  .delete(7, {delay: 500, speed: 120})
+  .type('Salut, m', { delay: 500, speed: 150 })
+  .move('END', { speed: 25, delay: 400 })
+  //   .delete(10)
+  .go();
